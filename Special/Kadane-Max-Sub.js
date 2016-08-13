4c2449354bac204f84d5d3531172024a9ef12568
draxem1 <nicklegato@gmail.com>
2016-08-13 17:00:40 -0400

    Kadane
    Finds sum of sub array.

---
+++ Special/Kadane-Max-Sub.js
@@ -0,0 +1,19 @@
+
+/****Finds the max of sub Array********/
+var maximumSubArray = function(array) {
+    var ans = 0;
+    var sum = 0;
+
+    console.log(ans, sum);
+    for (var i = 0; i < array.length; i++) {
+
+        ans = Math.max(0, ans + array[i]);
+        sum = Math.max(sum, ans);
+        console.log(ans, sum, array[i]);
+    }
+    console.log(ans, sum);
+    return sum;
+
+};
+
+maximumSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
